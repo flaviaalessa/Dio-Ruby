c6bc9be4a8825faea9a08d8de14bbad3a5167c62
flaviaalessa <85240386+flaviaalessa@users.noreply.github.com>
2023-07-16 16:09:05 -0300

    Primeiro projeto em Ruby
    Um sistema simples que pede os dados do usuário (nome, sobrenome e idade) e exibe para o usuário depois.

---
+++ desafiofrist.rb
@@ -0,0 +1,7 @@
+print "Digite seu nome"
+nome = gets.chomp
+print "Digite seu sobrenome"
+sobrenome = gets.chomp
+print "Digite sua idade"
+idade = gets.chomp.to_i
+print "Meu nome é #{nome} #{sobrenome} e minha idade é #{idade}"
