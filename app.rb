89217f75e7d85e60164c476290c82ce8632a31b5
flaviaalessa <85240386+flaviaalessa@users.noreply.github.com>
2023-07-21 10:53:31 -0300

    Delete app.rb
    Diretório invalido

--- app.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'produto'
-require_relative 'mercado'
- 
-produto = Produto.new
-produto.nome = 'Fatia de bolo de chocolate'
-produto.preco = 8.50
- 
-Mercado.new(produto.nome, produto.preco).comprar
